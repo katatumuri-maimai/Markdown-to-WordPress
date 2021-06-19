0b9a31f2c7182a8209bd55f230f1c40f2e0b1911
DESKTOP-TN66GDV\mymai <mm.katatumuri.48@gmail.com>
2021-06-19 19:34:34 +0900

    test

--- markdown2wp.py
+++ markdown2wp.py
@@ -59,8 +59,10 @@ for file in filelist:
         md = markdown.Markdown(extensions=["extra"])
         html = md.convert(text)
         print("html")
+        title= '<h1>*</h1>'
+        content= html.replace(title,'')
 
         # 記事を下書き投稿する（'draft'ではなく、'publish'にすれば公開投稿できます。）
-        post_article('draft', 'test-api-post', 'テストタイトルだよ', html, category_ids=[], tag_ids=[], media_id=None)
+        post_article('draft', 'test-api-post', title, content, category_ids=[], tag_ids=[], media_id=None)
 
 print("できた！")

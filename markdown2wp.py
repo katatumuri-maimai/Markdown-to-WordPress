6d82fe682bce97d540098899354717af5f85cd85
DESKTOP-TN66GDV\mymai <mm.katatumuri.48@gmail.com>
2021-06-19 18:01:33 +0900

    w

--- markdown2wp.py
+++ markdown2wp.py
@@ -6,10 +6,10 @@ import glob
 print("はじめるよ")
 
 filelist = glob.glob('posts/*.md')
-
-for i in filelist:
-    with open(filelist[i], mode='r', encoding='UTF-8') as fh:
-        text = fh.read()
-        md = markdown.Markdown()
-        html = md.convert(text)
-        print(html)
+print(filelist)
+# for i in filelist:
+    # with open(filelist[i], mode='r', encoding='UTF-8') as fh:
+    #     text = fh.read()
+    #     md = markdown.Markdown()
+    #     html = md.convert(text)
+    #     print(html)

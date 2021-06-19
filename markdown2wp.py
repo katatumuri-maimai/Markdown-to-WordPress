6558b635345e499c978f4c8164dc293075cc4065
DESKTOP-TN66GDV\mymai <mm.katatumuri.48@gmail.com>
2021-06-19 19:56:02 +0900

    aa

--- markdown2wp.py
+++ markdown2wp.py
@@ -70,6 +70,6 @@ for file in filelist:
         content_code=content.replace(code_header,'<!-- wp:codemirror-blocks/code-block --><div class="wp-block-codemirror-blocks-code-block code-block"><pre>').replace(code_fotter,'</pre></div><!-- /wp:codemirror-blocks/code-block -->').replace(code_html,'<!-- wp:codemirror-blocks/code-block {"mode":"htmlmixed","mime":"text/html"} --><div class="wp-block-codemirror-blocks-code-block code-block"><pre>')
 
         # 記事を下書き投稿する（'draft'ではなく、'publish'にすれば公開投稿できます。）
-        post_article('draft', 'test-api-post', title, content, category_ids=[], tag_ids=[], media_id=None)
+        post_article('draft', 'test-api-post', title, content_code, category_ids=[], tag_ids=[], media_id=None)
 
 print("できた！")

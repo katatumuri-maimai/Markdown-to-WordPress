2ebb064a3140e212517396a7d12a645be1929839
DESKTOP-TN66GDV\mymai <mm.katatumuri.48@gmail.com>
2021-06-22 21:47:19 +0900

    ヘッダーの項目を増やしました

--- markdown2wp.py
+++ markdown2wp.py
@@ -7,6 +7,7 @@ from urllib.parse import urljoin
 import glob
 from datetime import datetime, timedelta, timezone
 import re
+import random, string
 
 print("はじめるよ")
 
@@ -41,6 +42,13 @@ def post_article(status, slug, title, content, category_ids, tag_ids, media_id):
    print('＝＝＝＝＝＝「{}」の送信結果 → {}:{}＝＝＝＝＝＝'.format(title, repr(res.reason),repr(res.status_code)))
    return res
 
+
+#
+# ランダムに文字列を生成
+#
+def randomname(n):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=n))
+
 #
 # 読み込んだファイルをリストに変換してmd検出ファイルを検出
 #
@@ -59,45 +67,61 @@ def find_md_file(filelist_input):
 
     return filelist
 
+def find_article_header(file):
+    title =""
+    status_p = ""
+    status_b = ""
+    state = ""
+    slug = ""
+    category_ids = ""
+    tag_ids = ""
+    media_id = ""
+
+    with open(file, mode='r', encoding='UTF-8') as fh:
+        text = fh.read()
+        file_name = file.replace('.md','').replace('posts/','')
+        article_header = re.findall('>-+<.*?>-+<',text, flags=re.DOTALL) #識別番号増えた
+
+        if len(article_header[0])!=0:
+            header_line = re.match('>-+<',text).group()
+            heater_content = article_header[0].replace(header_line,'')
+            article_content = text.replace(article_header[0],'') # 記事の中身
+
+            # 記事情報の抽出
+            title = re.findall('\- タイトル\:\[(.*?)\]',heater_content, flags=re.DOTALL)[0]
+            status_p = re.findall('\- 投稿時\:p\[(.*?)\]公開d\[.*?\]下書き',heater_content, flags=re.DOTALL)[0]
+            status_b = re.findall('\- 投稿時\:p\[.*?\]公開d\[(.*?)\]下書き',heater_content, flags=re.DOTALL)[0]
+            state = status_p + status_b
+            slug = re.findall('\- カスタムURL\:\[(.*?)\]',heater_content, flags=re.DOTALL)[0]
+            category_ids = re.findall('\- カテゴリID\:\[(.*?)\]',heater_content, flags=re.DOTALL)[0]
+            tag_ids = re.findall('\- タグID\:\[(.*?)\]',heater_content, flags=re.DOTALL)[0]
+            media_id = re.findall('\- 見出し画像のID\:\[(.*?)\]',heater_content, flags=re.DOTALL)[0]
+
+            if len(title)==0:
+                title = file_name
+                print("タイトル情報がないので、ファイル名「{}」をタイトルにしました。".format(title))
+
+            if len(state)==0 | len(state)==2:
+                state = "draft"
+                print("下書き状態にしておきます。")
+
+            if len(slug)==0:
+                slug = file_name
+                print("カスタムURLがないので、ファイル名「{}」をカスタムURLにしました。".format(title))
+
+        else:
+            print("記事投稿情報がありません。ファイル名をタイトルにしますね！")
+            title = file_name
+
+        return title, state, slug, category_ids, tag_ids, media_id
 
 filelist = find_md_file(UPDATED_FILES)
 if len(filelist)!= 0 :
     for file in filelist:
         with open(file, mode='r', encoding='UTF-8') as fh:
             text = fh.read()
-            file_name = file.replace('.md','').replace('posts/','')
-            article_header = re.findall('>-+<.*?>-+<',text, flags=re.DOTALL)
-
-            if len(article_header[0])!=0:
-                header_line = re.match('>-+<',text).group()
-                heater_content = article_header[0].replace(header_line,'')
-                article_content = text.replace(article_header[0],'') # 記事の中身
-
-                # 記事情報の抽出
-                title = re.findall('\- タイトル\:\[(.*?)\]',heater_content, flags=re.DOTALL)[0]
-                status_p = re.findall('\- 投稿時\:p\[(.*?)\]公開d\[.*?\]下書き',heater_content, flags=re.DOTALL)[0]
-                status_b = re.findall('\- 投稿時\:p\[.*?\]公開d\[(.*?)\]下書き',heater_content, flags=re.DOTALL)[0]
-                state = status_p + status_b
-                slug = re.findall('\- カスタムURL\:\[(.*?)\]',heater_content, flags=re.DOTALL)[0]
-                category_ids = re.findall('\- カテゴリID\:\[(.*?)\]',heater_content, flags=re.DOTALL)[0]
-                tag_ids = re.findall('\- タグID\:\[(.*?)\]',heater_content, flags=re.DOTALL)[0]
-                media_id = re.findall('\- 見出し画像のID\:\[(.*?)\]',heater_content, flags=re.DOTALL)[0]
-
-                if len(title)==0:
-                    title = file_name
-                    print("タイトル情報がないので、ファイル名「{}」をタイトルにしました。".format(title))
-
-                if len(state)==0 | len(state)==2:
-                    state = "draft"
-                    print("下書き状態にしておきます。")
-
-                if len(slug)==0:
-                    slug = file_name
-                    print("カスタムURLがないので、ファイル名「{}」をカスタムURLにしました。".format(title))
-
-            else:
-                print("記事投稿情報がありません。ファイル名をタイトルにして投稿しますね！")
-                title = file_name
+            article_header = re.findall('>.*?>-+<.*?>-+<',text, flags=re.DOTALL)
+            print(article_header)
 
 
 if len(filelist)!= 0 :
